dfe5a7f8173e8f1e65edbcf92e3a064b0af70962
JoeVieyra <josevieyra@protonmail.com>
2024-12-31 19:20:22 -0600

    inicio1

---
+++ Variables.py
@@ -0,0 +1,28 @@
+print("hola mundo")
+#comentario
+"""
+comentario en varias lineas
+"""
+#variables
+#Es un contenedor para almacenar valores de datos.
+#Este contenedor va a tener un nombre.
+#Es creada la primera vez que se le asigna un valor.
+
+#   NOMBRES ADMITIDOS DE VARIABLES
+#Puede empezar con una letra o un guión bajo(underscore)
+
+mivariable ="texto"
+_mivariable = "otro texto"
+MiOtraVariable = "otro texto mas"
+
+print(mivariable)
+print(_mivariable)
+print(MiOtraVariable)
+
+x = 5
+y = 3
+txt = "esto es un texto"
+
+if  x   >   y: 
+    print(txt)
+  
--- proyecto.py
+++
@@ -1,3 +0,0 @@
-print("hola mundo")
-if  5   >   3:
-    print("5 es mayor que 3")
